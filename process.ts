c5589d89734d02f8e3250447ec5e4ea185de1b08
Jake O'Shannessy <joshannessy@gmail.com>
2024-09-01 18:00:17 +0100

    handle missing executables better (v0.1.9)

--- process.ts
+++ process.ts
@@ -6,14 +6,15 @@ export class SmokeviewProcess {
   private p: Deno.ChildProcess;
   private encoder: TextEncoder;
   public socketPath: string;
+  public output: Deno.CommandOutput | undefined;
   constructor(private smvPath: string) {
     const cmd = Deno.build.os === "windows" ? "smvlua.cmd" : "smvlua";
     this.socketPath = Deno.makeTempDirSync({ suffix: ".smv.socket.dir" });
     this.socketPath = path.join(this.socketPath, "socket");
     this.command = new Deno.Command(cmd, {
       stdin: "null",
-      stdout: "inherit",
-      stderr: "inherit",
+      stdout: "piped",
+      stderr: "piped",
       cwd: path.dirname(this.smvPath),
       args: [
         this.smvPath,
@@ -22,21 +23,19 @@ export class SmokeviewProcess {
       ],
     });
     this.p = this.command.spawn();
+    this.p.output().then((output) => this.output = output);
     this.p.ref();
     this.encoder = new TextEncoder();
   }
   async launch(): Promise<SmvRpc> {
     try {
-      // poll until socket file exists. We can't use stat to be compatible
-      // with windows.
-      let exists = false;
-      const socketDir = path.dirname(this.socketPath);
-      while (!exists) {
-        for await (const dirEntry of Deno.readDir(socketDir)) {
-          if (dirEntry.name === path.basename(this.socketPath)) {
-            exists = true;
-            break;
-          }
+      const socketExists = await this.waitForSocket();
+      if (!socketExists) {
+        if (this.output) {
+          const stderr = (new TextDecoder()).decode(this.output.stderr);
+          throw new Error(`no socket: "${stderr}"`);
+        } else {
+          throw new Error("Could not establish socket");
         }
       }
       return new SmvRpc(this);
@@ -44,6 +43,21 @@ export class SmokeviewProcess {
       this.close();
       throw e;
     }
+  }
+  async waitForSocket(): Promise<boolean> {
+    let exists = false;
+    const socketDir = path.dirname(this.socketPath);
+    // poll until socket file exists. We can't use stat to be compatible
+    // with windows.
+    while (!exists && this.output === undefined) {
+      for await (const dirEntry of Deno.readDir(socketDir)) {
+        if (dirEntry.name === path.basename(this.socketPath)) {
+          exists = true;
+          break;
+        }
+      }
+    }
+    return exists;
   }
   close() {
     // console.error("killing");
